334a3ee051d4a87a909e3912d16a5ec689efeade
Ibrahim_Mrq <46426865+ibrahim-mrq@users.noreply.github.com>
2021-04-27 13:13:27 +0300

    update color picker dialog

--- app/src/main/java/com/mrq/library/MainActivity.java
+++ app/src/main/java/com/mrq/library/MainActivity.java
@@ -3,6 +3,8 @@ package com.mrq.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
